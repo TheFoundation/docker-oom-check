5d7256c55b8cfe61e7ea831d8391990e0734ecd3
Benjamin Freitag <12718580+benchonaut@users.noreply.github.com>
2024-09-25 13:05:39 +0200

    Create check-oom.sh

---
+++ check-oom.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+# the first argument has to be a bash script to which we will pipe the message
+grep  "$(date "+%b %d %T"|cut -d":" -f1)" /var/log/syslog |grep -q OOM && ( 
+
+       (echo Out-Of-Memory found in /var/log/syslog; 
+       echo "System status:"
+       echo
+       docker stats --no-stream | tr -d "%" | awk '{if($7>80)print "Warning: " $1 "/" $2" uses over 80%  RAM ("$7"%)"}'
+       echo
+       echo "OOM_Incidents ( last 3 ):"
+       echo
+       grep OOM /var/log/syslog|grep "$(date "+%b %d %T"|cut -d":" -f1)"|tail -n3;
+       echo 
+       echo "containers ever restarted"
+       docker ps -a --format '{{.Names}}'|while read a ;do docker container inspect $a| jq .[].State.OOMKilled|grep true -q && echo "OOM $a";done
+       ) | bash "$1"
+)
